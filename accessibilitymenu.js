8bd076258f7cab6c1684433b3ecb7cdf5f56af0a
DSMPhillips <129297874+DSMPhillips@users.noreply.github.com>
2024-01-11 14:28:16 -0500

    Update accessibilitymenu.js

--- accessibilitymenu.js
+++ accessibilitymenu.js
@@ -4,7 +4,12 @@ videojs.registerPlugin('AccessibilityButton', function() {
   // +++ Create divs for buttons +++
   var vPlayer = this,
     controlBar,
-    insertBeforeNode;
+    insertBeforeNode,
+    newElementAM = document.createElement("div");
+
+  // +++ Assign properties to elements and assign to parents +++
+    newElementAM.innerHTML = "<button class='vjs-control vjs-button vjs-accessibilty' type='button' title='Accessibility Menu' aria-disabled='false'><span class='vjs-icon-placeholder' aria-hidden='true'></span><span class='vjs-control-text' aria-live='polite'>Select Captions and Descriptive Audio</span></button>";
+
     AccessibilityButton.addChild('AccessibilityMenu');
     AccessibilityButton.addChild('SubsCapsMenuItem');
     AccessibilityButton.addChild('CaptionSettingsMenuItem');
@@ -15,4 +20,8 @@ videojs.registerPlugin('AccessibilityButton', function() {
   controlBar = vPlayer.$(".vjs-control-bar");
   // Get the element to insert buttons in front of in conrolbar
   insertBeforeNode = vPlayer.$(".vjs-fullscreen-toggle");
+
+   // Insert the button div in proper location
+  controlBar.insertBefore(newElementAM, insertBeforeNode);
+
 });
