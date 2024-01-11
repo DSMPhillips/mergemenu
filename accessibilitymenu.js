be27944b2771491fcdb70e90ded591a9cf50e61c
DSMPhillips <129297874+DSMPhillips@users.noreply.github.com>
2024-01-11 14:07:12 -0500

    Update and rename merge-menu.js to accessibilitymenu.js

---
+++ accessibilitymenu.js
@@ -0,0 +1,16 @@
+var AccessibilityButton = player.getChild('ControlBar').addChild('Accessibility Options', {
+  clickHandler: function(event) {
+    videojs.log('Clicked');
+  }
+});
+
+AccessibilityButton.setIcon('menu');
+
+var AccessibilityMenu = new Component(player);
+AccessibilityButton.addChild('AccessibilityMenu');
+AccessibilityMenu.addChild('SubsCapsMenuItem');
+AccessibilityMenu.addChild('CaptionSettingsMenuItem');
+AccessibilityMenu.addChild('OffTextTrackMenuItem');
+AccessibilityMenu.addChild('AudioTrackMenuItem');
+
+console.log(button.el());
