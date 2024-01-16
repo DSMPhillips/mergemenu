37add4b63936ac9e6a4aae6823f372f2481983c1
DSMPhillips <129297874+DSMPhillips@users.noreply.github.com>
2024-01-16 10:09:57 -0500

    Update AccessibilityButton.js

--- AccessibilityButton.js
+++ AccessibilityButton.js
@@ -1,41 +1,28 @@
-var MenuButton = videojs.getComponent('MenuButton');
-var MenuItem = videojs.getComponent('MenuItem');
+	var player = videojs('videojs-settings-menu-player', {
+		'playbackRates': [0.5, 1, 1.5, 2],
+		controlBar: {
+			children: {
+				'playToggle':{},
+				'muteToggle':{},
+				'volumeControl':{},
+				'currentTimeDisplay':{},
+				'timeDivider':{},
+				'durationDisplay':{},
+				'liveDisplay':{},
 
-class AccessibilityButton extends MenuButton
-  {
-    createItems: function() {
-      // Must return an array of `MenuItem`s
-      // Options passed in `addChild` are available at `this.options_`
-      AccessibilityButton.addChild('AccessibilityMenu');
-      AccessibilityButton.addChild('SubsCapsMenuItem');
-      AccessibilityButton.addChild('CaptionSettingsMenuItem');
-      AccessibilityButton.addChild('OffTextTrackMenuItem');
-      AccessibilityButton.addChild('AudioTrackMenuItem');
-      return this.options().myItems.map(function(i) {
-        var item = new MenuItem(this.player(), {label: i.name});
-        item.handleClick = function() { /* ... */ };
-        return item;
-      });
-    }
-  }
-);
+				'flexibleWidthSpacer':{},
+				'progressControl':{},
 
-// Register as a component, so it can be added
-videojs.registerComponent('AccessibilityButton', AccessibilityButton);
-var vPlayer = this,
-    controlBar,
-    insertBeforeNode;
-
-// +++ Get controlbar and insert elements +++
-  controlBar = vPlayer.$(".vjs-control-bar");
-  // Get the element to insert buttons in front of in conrolbar
-  insertBeforeNode = vPlayer.$(".vjs-picture-in-picture-control");
-
-  // Insert the button div in proper location
-  controlBar.insertBefore(AccessibilityButton, insertBeforeNode);
-
-// Use `addChild` to add an instance of the new component, with options
-player.controlBar.addChild('AccessibilityButton', {
-  title: 'Accessibility Menu',
-  myItems: [{name: 'Hello'}, {name: 'World'}]
-});
+				'settingsMenuButton': {
+					entries : [
+						'subtitlesButton',
+            'CaptionSettingsMenuItem',
+            'OffTextTrackMenuItem',
+						'SubsCapsMenuItem',
+            'AudioTrackMenuItem'
+					]
+				},
+				'fullscreenToggle':{}
+			}
+		}
+	});
